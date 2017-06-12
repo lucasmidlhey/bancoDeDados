7d56b9df3b7127d82edb7eecda91df95ed7a2f6b
Ícaro Oliveira <icarooliv2@gmail.com>
2017-06-11 22:10:39 -0300

    revised scripts

---
+++ insert.sql
@@ -0,0 +1,9 @@
+-- insert values in table Fornecedor
+
+insert into Fornecedor (id_fornecedor, nome_fornecedor, descricao_fornecedor, cnpj_fornecedor)  values (001, 'empresa de cimento 001', 'especializada em fornecer cimento', 12345678912345);
+insert into Fornecedor (id_fornecedor, nome_fornecedor, descricao_fornecedor, cnpj_fornecedor)  values (002, 'Empresa Tampa buraco', 'fornecer produtos e ferramentas', 12345678912343);
+insert into Fornecedor (id_fornecedor, nome_fornecedor, descricao_fornecedor, cnpj_fornecedor)  values (003, 'Ferramentas BR', 'especializada em fornecer ferramentas', 12345678912344);
+
+-- insert values in table Departamento
+
+insert into Departamento values (001, 'Departamento de Obras Urbanas');
